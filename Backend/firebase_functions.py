2287417e1a9f96f25cdde77616c44e7c45571bd7
nmofficial <nmv.co.uk@gmail.com>
2020-12-05 00:56:43 -0500

    hi

--- Backend/firebase_functions.py
+++ Backend/firebase_functions.py
@@ -1,5 +1,27 @@
-from firebase import firebase
+from firebase import Firebase
 
+apiKey: "AIzaSyBathSfk_1F36efi-zc5DlwKlHvrHG_ors",
+    authDomain: "nsgt-5ed5a.firebaseapp.com",
+    databaseURL: "https://nsgt-5ed5a-default-rtdb.firebaseio.com",
+    projectId: "nsgt-5ed5a",
+    storageBucket: "nsgt-5ed5a.appspot.com",
+    messagingSenderId: "436519156337",
+    appId: "1:436519156337:web:e4f44e253153cc80bdb9d0",
+    measurementId: "G-CHDNNE6FVT" 
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "projectId.firebaseapp.com",
+  "databaseURL": "https://databaseName.firebaseio.com",
+  "storageBucket": "projectId.appspot.com",
+  "serviceAccount": "path/to/serviceAccountCredentials.json"
+}
+
+firebase = Firebase(config)
+db = firebase.database()
+
+
+'''
 firebase = firebase.FirebaseApplication("https://nsgt-5ed5a-default-rtdb.firebaseio.com/", None)
 
 data = {
@@ -11,3 +33,4 @@ data = {
 }
 
 result = firebase.post('/nsgt-5ed5a-default-rtdb/Users', data)
+'''
