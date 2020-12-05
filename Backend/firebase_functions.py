3ad2455300c389adbdf3a2a09a8ae072ae501b26
Amaya Howard-Carswell <akh2161@columbia.edu>
2020-12-05 17:13:49 -0500

    Fixed up the formatting

--- Backend/firebase_functions.py
+++ Backend/firebase_functions.py
@@ -1,19 +1,19 @@
 from firebase import Firebase
 
-apiKey: "AIzaSyBathSfk_1F36efi-zc5DlwKlHvrHG_ors",
-    authDomain: "nsgt-5ed5a.firebaseapp.com",
-    databaseURL: "https://nsgt-5ed5a-default-rtdb.firebaseio.com",
-    projectId: "nsgt-5ed5a",
-    storageBucket: "nsgt-5ed5a.appspot.com",
-    messagingSenderId: "436519156337",
-    appId: "1:436519156337:web:e4f44e253153cc80bdb9d0",
-    measurementId: "G-CHDNNE6FVT" 
+apiKey = "AIzaSyBathSfk_1F36efi-zc5DlwKlHvrHG_ors"
+authDomain = "nsgt-5ed5a.firebaseapp.com"
+databaseURL =  "https://nsgt-5ed5a-default-rtdb.firebaseio.com"
+projectId = "nsgt-5ed5a"
+storageBucket = "nsgt-5ed5a.appspot.com"
+messagingSenderId = "436519156337"
+appId = "1:436519156337:web:e4f44e253153cc80bdb9d0"
+measurementId = "G-CHDNNE6FVT"
 
 config = {
-  "apiKey": "apiKey",
-  "authDomain": "projectId.firebaseapp.com",
-  "databaseURL": "https://databaseName.firebaseio.com",
-  "storageBucket": "projectId.appspot.com",
+  "apiKey": apiKey,
+  "authDomain": "{}.firebaseapp.com".format(projectId),
+  "databaseURL": databaseURL,
+  "storageBucket": storageBucket,
   "serviceAccount": "path/to/serviceAccountCredentials.json"
 }
 
@@ -52,7 +52,7 @@ db.child("answers").push(a5)
 db.child("answers").push(a6)
 db.child("answers").push(a7)
 db.child("answers").push(a8)
-db.child("answers:wq::").push(a9)
+db.child("answers").push(a9)
 
 
 
